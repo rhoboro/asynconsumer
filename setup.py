388f723acf9fce497104f10eb6c42238cc49bf7d
rhoboro <rhoboro@gmail.com>
2018-07-14 18:28:00 +0900

    バージョンを1.0.0に

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def main():
     setup(
         name='asynconsumer',
-        version='0.0.1',
+        version='1.0.0',
         url='https://github.com/rhoboro/asynconsumer',
         license='Apache License 2.0',
         author='rhoboro',
